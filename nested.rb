e3c99ef3419238d981a69e80912099e65711ca84
Sarah Allrozamo <sarahallrozamo@gmail.com>
2020-09-20 19:31:13 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -106,5 +106,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages].include? ("Assembly")
+     
+  dennis_new_info = "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << dennis_new_info
+  programmer_hash
 end
